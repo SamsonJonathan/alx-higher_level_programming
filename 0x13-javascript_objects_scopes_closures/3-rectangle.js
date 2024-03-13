e1c1f37a6ad269a2ab605720c07c313dc7ee1fab
samsonjonathan <sj204259@.com>
2024-03-13 13:29:42 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,8 @@ class Rectangle {
       this.height = h;
     }
   }
-   print () {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
       let line = '';
       for (let j = 0; j < this.width; j++) {
